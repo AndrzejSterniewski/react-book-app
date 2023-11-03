dc2d9f11e50c7b857ce5c3394bee617ee694b7fa
Andrzej Sterniewski <drapiszon@gmail.com>
2023-11-03 21:57:18 +0100

    Add redux to application

---
+++ src/App - class component.js
@@ -0,0 +1,37 @@
+// import BookForm from "./components/BookForm/BookForm";
+// import BooksList from "./components/BooksList/BooksList";
+// import React, { useState } from "react";
+// import shortid from "shortid";
+
+// class App extends React.Component {
+
+//   state = {
+//     books: [
+//       { id: 1, title: 'of Mice and Men', author: 'John Steinbeck' },
+//       { id: 2, title: 'the Witcher', author: 'Andrzej Sapkowski' }
+//     ]
+//   };
+
+//   removeBook = bookId => {
+//     this.setState({ books: this.state.books.filter(book => book.id !== bookId) })
+//   };
+
+//   addBook = newBook => {
+//     this.setBooks({ books: [...this.state.books, { id: shortid(), title: newBook.title, author: newBook.author }] });
+//   }
+
+//   render() {
+//     const { books } = this.state;
+//     const { addBook, removeBook } = this;
+
+//     return (
+//       <div className="App">
+//         <h1>Books App</h1>
+//         <BooksList books={books} removeBook={removeBook} />
+//         <BookForm addBook={addBook} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
---
+++ src/components/BookForm/BookForm.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+const BookForm = () => {
+
+    const dispatch = useDispatch();
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({ type:'ADD_BOOK', payload: { title, author } });
+        setTitle('');
+        setAuthor('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            Author: <input type="text" value={author} onChange={e => setAuthor(e.target.value)} />
+            <button>Add book</button>
+        </form>
+    );
+};
+
+export default BookForm;
+
+// Class component
+
+// import React from 'react';
+
+// class BookForm extends React.Component {
+
+//     state = {
+//         title: '',
+//         author: ''
+//     };
+
+//     handleSubmit = e => {
+//         e.preventDefault();
+//         this.props.addBook({ title: this.state.title, author: this.state.author });
+//         this.setState({ ...this.state, title: '', author: '' });
+//     }
+
+//     setTitle = value => {
+//         this.setState({ ...this.state, title: value })
+//     }
+
+//     setAuthor = value => {
+//         this.setState({ ...this.state, author: value })
+//     }
+
+//     render() {
+//         const { handleSubmit, setTitle, setAuthor } = this;
+//         const { title, author } = this.state;
+
+//         return (
+//             <form onSubmit={handleSubmit}>
+//                 Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+//                 Author: <input type="text" value={author} onChange={e => setAuthor(e.target.value)} />
+//                 <button>Add book</button>
+//             </form>
+//         );
+//     }
+// };
+
+// export default BookForm;
