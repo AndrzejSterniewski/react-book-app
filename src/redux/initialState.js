97e8ec947667829db26b28dbdb9dfdc305a13d31
Andrzej Sterniewski <drapiszon@gmail.com>
2023-11-29 23:52:47 +0100

    add booksRedux, add connecting with server

---
+++ src/redux/initialState.js
@@ -0,0 +1,9 @@
+const initialState = {
+    books: [
+        { id: 1, title: 'of Mice and Men', author: 'John Steinbeck' },
+        { id: 2, title: 'the Witcher', author: 'Andrzej Sapkowski' },
+        { id: 3, title: 'Hobbit', author: 'J.R.R. Tolkien' }
+    ]
+};
+
+export default initialState;
